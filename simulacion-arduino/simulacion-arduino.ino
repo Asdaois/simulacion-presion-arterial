59fbeb763b28407f385fe8de92177509f3d613e2
Jose Guevara <94gjose@gmail.com>
2023-04-08 21:34:33 -0400

    feat: agregar valoes de presion minimos y maximos

--- simulacion-arduino/simulacion-arduino.ino
+++ simulacion-arduino/simulacion-arduino.ino
@@ -5,9 +5,6 @@
 #include "pulsador_con_rebote.h"
 #include "valor_simulacion.h"
 
-#define BPM_MIN 40
-#define BPM_MAX 120
-
 int indice_actual = 0;
 unsigned long milisegundos_desde_se_mostro_onda = 0;
 unsigned long milisegundos_desde_se_mostro_datos = 0;
@@ -18,10 +15,9 @@ Adafruit_MCP4725 dac;
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
-ValorSimulacion bpm(60, BPM_MIN, BPM_MAX, 0, 1);
-ValorSimulacion presion_minima(80, BPM_MIN, BPM_MAX, 9, 8);
-ValorSimulacion presion_maxima(120, BPM_MIN, BPM_MAX, 11, 10);
-ValorSimulacion dispositivo_medida_maxima(150, BPM_MIN, BPM_MAX, 13, 12);
+ValorSimulacion bpm(60, FRECUENCIA_CARDIACA_MINIMA, FRECUENCIA_CARDIACA_MAXIMA, 0, 1);
+ValorSimulacion presion_minima(80, PRESION_DISTOLICA_MINIMA, PRESION_DISTOLICA_MAXIMA, 9, 8);
+ValorSimulacion presion_maxima(120, PRESION_SISTOLICA_MINIMA, PRESION_SISTOLICA_MAXIMA, 11, 10);
 
 void setup() {
   dac.begin(0x60);
@@ -31,7 +27,6 @@ void setup() {
   bpm.setup();
   presion_minima.setup();
   presion_maxima.setup();
-  dispositivo_medida_maxima.setup();
 }
 
 void loop() {
@@ -48,7 +43,6 @@ void loop() {
   bpm.loop();
   presion_minima.loop();
   presion_maxima.loop();
-  dispositivo_medida_maxima.loop();
 }
 
 void mostrar_sennal_cardiaca() {
@@ -61,7 +55,7 @@ void mostrar_sennal_cardiaca() {
 
 void mostrar_frecuencia_cardiaca() {
   lcd.setCursor(0, 0);
-  lcd.print("BPM:" + String(bpm.obtener_valor()) + " DISP:" + String(dispositivo_medida_maxima.obtener_valor()));
+  lcd.print("BPM:" + String(bpm.obtener_valor()));
   lcd.setCursor(0, 1);
   lcd.print("MIN:" + String(presion_minima.obtener_valor()) + " MAX:" + String(presion_maxima.obtener_valor()));
 }
