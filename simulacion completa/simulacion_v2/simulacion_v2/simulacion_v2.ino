0d8c35726a3b492f0c30f13cdf6d2afa9a73fc34
Jose Guevara <94gjose@gmail.com>
2024-11-11 20:33:30 -0400

    agregar funcionalidad on hold

--- simulacion completa/simulacion_v2/simulacion_v2/simulacion_v2.ino
+++ simulacion completa/simulacion_v2/simulacion_v2/simulacion_v2.ino
@@ -24,6 +24,7 @@
 typedef unsigned int uint;
 
 uint ultimo_tiempo_presionado = 0;
+char key_sostenida = '#';
 
 enum PantallaActual {
   Presion,
@@ -43,6 +44,7 @@ void setup() {
 
 void loop() {
   char key = teclado_obtener_key();
+  manejar_key_sostenida();
 }
 
 void pantalla_mostrar(String linea1, String linea2, String linea3, String linea4) {
@@ -141,9 +143,11 @@ void teclado_evento(char key) {
       break;
 
     case RELEASED:
+      key_sostenida = '#';
       break;
 
     case HOLD:
+      key_sostenida = key;
       break;
 
     default:
@@ -178,5 +182,102 @@ void menu_abajo() {
     case PantallaActual::TemperaturaYSO2:
       pantalla_presion_mostrar();
       break;
+  }
+}
+
+void manejar_key_sostenida() {
+  static uint tiempo_actual = 0;
+  static uint tiempo_previo = 0;
+
+  tiempo_actual = millis();
+  if (key_sostenida == '#') { return; }
+
+  if ((tiempo_actual - tiempo_previo) < 50) {
+    return;
+  }
+
+  tiempo_actual = tiempo_previo;
+  switch (key_sostenida) {
+      // case ECG_NORMAL:
+      //   ecg_cambiar_estado_normal();
+      //   Serial.println("Tecla presionada: ECG Normal");
+      //   break;
+
+      // case ECG_SOBLO_CORAZON:
+      //   ecg_cambiar_estado_soplo();
+      //   Serial.println("Tecla presionada: ECG Soblo Corazón");
+      //   break;
+      // case ECG_ARRITMIA:
+      //   ecg_cambiar_estado_arritmia();
+      //   Serial.println("Tecla presionada: ECG Arritmia");
+      //   break;
+
+      // case POR_DEFINIR:
+      //   pantalla_mostrar("Por Definir", "", "", "");
+      //   Serial.println("Tecla presionada: Por Definir");
+      //   break;
+
+    case PRESION_DISTOLICA_SUBIR:
+      presion_distolica_aumentar();
+      Serial.println("Tecla presionada: Presion Diastolica Subir");
+      break;
+
+    case PRESION_DISTOLICA_BAJAR:
+      presion_distolica_disminuir();
+      Serial.println("Tecla presionada: Presion Diastólica Bajar");
+      break;
+
+    case PRESION_SISTOLICA_SUBIR:
+      presion_sistolica_aumentar();
+      Serial.println("Tecla presionada: Presion Sistolica Subir");
+      break;
+
+    case PRESION_SISTOLICA_BAJAR:
+      presion_sistolica_disminuir();
+      Serial.println("Tecla presionada: Presion Sistolica Bajar");
+      break;
+
+    case BMP_SUBIR:
+      bpm_subir();
+      Serial.println("Tecla presionada: BMP Subir");
+      break;
+
+    case BMP_BAJAR:
+      bpm_bajar();
+      Serial.println("Tecla presionada: BMP Bajar");
+      break;
+
+      // case MENU_SUBIR:
+      //   menu_arriba();
+      //   Serial.println("Tecla presionada: Menú Subir");
+      //   break;
+
+      // case MENU_BAJAR:
+      //   menu_abajo();
+      //   Serial.println("Tecla presionada: Menú Bajar");
+      //   break;
+
+    case TEMPERATURA_SUBIR:
+      temperatura_aumentar();
+      Serial.println("Tecla presionada: Temperatura Subir");
+      break;
+
+    case TEMPERATURA_BAJAR:
+      temperatura_disminuir();
+      Serial.println("Tecla presionada: Temperatura Bajar");
+      break;
+
+    case SO2_SUBIR:
+      spo2_aumentar();
+      Serial.println("Tecla presionada: SO2 Subir");
+      break;
+
+    case SO2_BAJAR:
+      spo2_disminuir();
+      Serial.println("Tecla presionada: SO2 Bajar");
+      break;
+
+    default:
+      Serial.println("Tecla no valida on hold");
   }
 }
