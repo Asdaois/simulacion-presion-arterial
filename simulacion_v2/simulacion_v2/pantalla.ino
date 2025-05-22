cb5c66ee56cbf3ff1b726d69b610d4c1eec36d51
Jose Guevara <94gjose@gmail.com>
2025-05-22 13:07:16 -0400

    arreglar bug en la pantalla
    se esta mostrando el caracter de retorno de nueva linea en esta (comportamiento no deseado)

--- simulacion_v2/simulacion_v2/pantalla.ino
+++ simulacion_v2/simulacion_v2/pantalla.ino
@@ -13,11 +13,11 @@ void pantalla_configurar() {
 void pantalla_mostrar_implementacion(String linea1, String linea2, String linea3, String linea4) {
   pantalla.clear();
   pantalla.setCursor(0, 0);
-  pantalla.println(linea1);
+  pantalla.print(linea1);
   pantalla.setCursor(0, 1);
-  pantalla.println(linea2);
+  pantalla.print(linea2);
   pantalla.setCursor(0, 2);
-  pantalla.println(linea3);
+  pantalla.print(linea3);
   pantalla.setCursor(0, 3);
-  pantalla.println(linea4);
+  pantalla.print(linea4);
 }
